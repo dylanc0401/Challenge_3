204c48e6938929644b98fa026a7cbab2a0047159
dylanc0401 <dylanc.0401@gmail.com>
2019-03-19 16:46:50 +1100

    attempted

--- challenge_3/Program.cs
+++ challenge_3/Program.cs
@@ -10,6 +10,62 @@ namespace challenge_3
     {
         static void Main(string[] args)
         {
+
+            
+
+            Console.WriteLine("Would you like to start the quiz");
+            int cor = 0;
+            string input = Console.ReadLine().ToLower();
+            if (input == "yes")
+            {
+                Console.WriteLine("You have 10 seconds to answer each of these 10 questions");
+                Console.WriteLine("when you are ready please type 'start'");
+            }
+            else
+            {
+                Console.WriteLine("Please try again later");
+                Console.Read();
+                return;
+            }
+
+            input = Console.ReadLine().ToLower();
+
+            if (input== "start")
+            {
+                for (int i = 0; i < 9; i++)
+                {
+                    Random rnd = new Random();
+                    int num_1 = rnd.Next(1, 100);
+                    int num_2 = rnd.Next(1, 100);
+                    int answer = num_1 + num_2;
+                    Console.WriteLine("Q what does " + num_1 + " + " + num_2 + " equal");
+
+                    DateTime t1 = DateTime.Now;
+                    String guess = Console.ReadLine();
+                    DateTime t2 = DateTime.Now;
+
+                    int seconds = (t2 - t1).Seconds;
+                    Console.WriteLine(seconds + " seconds");
+                    if (guess == answer.ToString() && seconds <= 10 )
+                    {
+                        Console.WriteLine("correct");
+                        cor++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("incorrect or took too long!");
+                    }
+                }
+                
+            }
+            else
+            {
+                Console.WriteLine("Please try again later");
+                Console.Read();
+                return;
+            }
+            Console.WriteLine("you got " + cor + " out of 10 correct");
+            Console.Read();
         }
     }
 }
